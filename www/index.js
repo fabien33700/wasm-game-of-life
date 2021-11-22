44acd7f24fe0373121711d8abbdea03b97fd09a5
Fabien LH <fabien.lehouedec@gmail.com>
2021-11-22 22:37:46 +0100

    Added www files instead of submodule

---
+++ www/index.js
@@ -0,0 +1,141 @@
+import { Universe, Cell } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
+
+
+const CELL_SIZE = 5;
+const GRID_COLOR = "#ddd";
+const DEAD_COLOR = "white";
+const ALIVE_COLOR = "black";
+
+const width = Math.floor(window.innerWidth / (CELL_SIZE + 1));
+const height = Math.floor(window.innerHeight / (CELL_SIZE + 1));
+
+// Construct the universe, and get its width and height.
+const universe = Universe.new(width, height);
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+  universe.tick();
+  draw();
+
+  requestAnimationFrame(renderLoop);
+};
+
+
+document.getElementById('btn_annihilate').addEventListener('click', () => {
+  universe.annihilate();
+  draw();
+});  
+
+document.getElementById('btn_reset').addEventListener('click', () => {
+  universe.reset();
+  draw();
+});
+
+canvas.addEventListener('click', event => {
+  const { row, col } = mouseToUniverseCoords(event);
+
+  if (event.ctrlKey)
+    universe.pop_glider(row, col);
+  else
+    universe.toggle_cell(row, col);
+
+  draw();
+});
+
+const mouseToUniverseCoords = (event) => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  return { row, col }
+}
+
+const draw = () => {
+  drawGrid();
+  drawCells();
+}
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const getIndex = (row, column) => {
+  return row * width + column;
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  // Alive cells
+  ctx.fillStyle = ALIVE_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (!cells[idx]) continue;
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  // Dead cells
+  ctx.fillStyle = DEAD_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+      if (cells[idx]) continue;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+renderLoop();
